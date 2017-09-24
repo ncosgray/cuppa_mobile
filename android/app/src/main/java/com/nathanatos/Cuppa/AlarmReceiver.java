5fb2d66ef287e2cab0b71e28f9bd86ab8063e07e
ncosgray <ncosgray@gmail.com>
2017-09-24 13:38:46 -0700

    notification channel

--- android/app/src/main/java/com/nathanatos/Cuppa/AlarmReceiver.java
+++ android/app/src/main/java/com/nathanatos/Cuppa/AlarmReceiver.java
@@ -1,8 +1,11 @@
 package com.nathanatos.Cuppa;
 
+import android.os.Build;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.NotificationChannel;
 import android.app.PendingIntent;
+import android.graphics.Color;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -17,24 +20,38 @@ public class AlarmReceiver extends BroadcastReceiver {
         Intent resultIntent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
+        // Set up notification
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder = null;
+        NotificationChannel notificationChannel = null;
 
         // Configure the notification channel (requirement when targeting Android O+)
-        //NotificationChannel notificationChannel = new NotificationChannel(CHANNEL,
-        //        "Cuppa Timer",
-        //        NotificationManager.IMPORTANCE_HIGH);
-        //notificationChannel.setDescription("Notifications for brewing completion");
-        //notificationChannel.enableLights(true);
-        //notificationChannel.setLightColor(Color.GREEN);
-        //notificationChannel.enableVibration(true);
-        //notificationManager.createNotificationChannel(notificationChannel);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            notificationChannel = new NotificationChannel(CHANNEL,
+                    "Cuppa Timer",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setDescription("Notifications for brewing completion");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.GREEN);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setShowBadge(true);
+            notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            // Build notification with channel
+            builder = new Notification.Builder(context, CHANNEL);
+        }
+        else
+        {
+            // Build notification without channel for pre-O
+            builder = new Notification.Builder(context);
+        }
 
         // Create the notification
-        Notification.Builder builder = new Notification.Builder(context);
         Notification notification = builder.setContentTitle("Brewing complete")
                 .setContentText("Your tea is now ready!")
                 .setSmallIcon(R.drawable.ic_stat_name)
-                //.setChannel(CHANNEL)
                 .setPriority(Notification.PRIORITY_HIGH)
                 .setContentIntent(pendingIntent).build();
         notification.flags |= Notification.FLAG_AUTO_CANCEL;
