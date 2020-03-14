92666c0e0108a71d2f667f7f62ebf21cb752450d
ncosgray <ncosgray@gmail.com>
2020-03-14 14:08:24 -0700

    header comments, copyright/legal text

--- lib/platform_adaptive.dart
+++ lib/platform_adaptive.dart
@@ -1,9 +1,42 @@
-// Cuppa platform_adaptive.dart
-// Author: Nathan Cosgray | https://www.nathanatos.com
+/*
+ *******************************************************************************
+ Package:  cuppa_mobile
+ Class:    platform_adaptive.dart
+ Author:   Nathan Cosgray | https://www.nathanatos.com
+ -------------------------------------------------------------------------------
+ Copyright (c) 2017-2020 Nathan Cosgray. All rights reserved.
 
-// Light and dark themes for Android and iOS
-// PlatformAdaptiveAppBar from https://github.com/efortuna/memechat
-// PlatformAdaptiveDialog chooses showDialog type by context platform
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1.  Redistributions of source code must retain the above copyright notice, this
+     list of conditions and the following disclaimer.
+
+ 2.  Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+ 3.  Neither the name of Nathanatos Software nor the names of its contributors
+     may be used to endorse or promote products derived from this software
+     without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COMPANY OR CONTRIBUTORS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************
+*/
+
+// Cuppa platform adaptive elements
+// - Light and dark themes for Android and iOS
+// - PlatformAdaptiveAppBar from https://github.com/efortuna/memechat
+// - PlatformAdaptiveDialog chooses showDialog type by context platform
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
--- lib/timer.dart
+++ lib/timer.dart
@@ -1,5 +1,43 @@
-// Cuppa timer.dart
-// Author: Nathan Cosgray | https://www.nathanatos.com
+/*
+ *******************************************************************************
+ Package:  cuppa_mobile
+ Class:    timer.dart
+ Author:   Nathan Cosgray | https://www.nathanatos.com
+ -------------------------------------------------------------------------------
+ Copyright (c) 2017-2020 Nathan Cosgray. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1.  Redistributions of source code must retain the above copyright notice, this
+     list of conditions and the following disclaimer.
+
+ 2.  Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+ 3.  Neither the name of Nathanatos Software nor the names of its contributors
+     may be used to endorse or promote products derived from this software
+     without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COMPANY OR CONTRIBUTORS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************
+*/
+
+// Cuppa timer widgets and logic
+// - Tea definitions
+// - Build interface and interactivity
+// - Start, confirm, cancel timers
+// - Notification channels for platform code
 
 import 'dart:async';
 import 'package:flutter/foundation.dart';
@@ -8,8 +46,6 @@ import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:quick_actions/quick_actions.dart';
 import 'platform_adaptive.dart';
-
-// Cuppa: timer classes
 
 class TimerWidget extends StatefulWidget {
   @override
