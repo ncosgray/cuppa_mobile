db4c7eb5d0b1e3733957885f60588e0ad0b05820
ncosgray <ncosgray@gmail.com>
2021-08-07 13:29:34 -0700

    more Celsius temp options

--- lib/prefs.dart
+++ lib/prefs.dart
@@ -511,9 +511,13 @@ class _PrefsTeaRowState extends State<PrefsTeaRow> {
                             underline: SizedBox(),
                             items: (<int>[
                               60,
+                              65,
                               70,
+                              75,
                               80,
+                              85,
                               90,
+                              95,
                               100,
                               140,
                               150,
