b040e877ce7f9544a743c2f6c9ae918d70b3556f
ncosgray <ncosgray@gmail.com>
2023-07-23 10:11:45 -0700

    Urdu localization

--- lib/data/localization.dart
+++ lib/data/localization.dart
@@ -49,6 +49,7 @@ final Map<Locale, String> supportedLocales = {
   const Locale.fromSubtags(languageCode: 'ru'): 'Русский',
   const Locale.fromSubtags(languageCode: 'uk'): 'Українська',
   const Locale.fromSubtags(languageCode: 'he'): 'עברית',
+  const Locale.fromSubtags(languageCode: 'ur'): 'اردو',
   const Locale.fromSubtags(languageCode: 'zh'): '简体中文',
   const Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hant'): '繁體中文',
 };
