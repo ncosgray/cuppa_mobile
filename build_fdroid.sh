d601e5a616d3eee073b7ffa79c1796f5a70351be
ncosgray <ncosgray@gmail.com>
2025-09-09 18:00:42 -0700

    build script improvements

--- build_fdroid.sh
+++ build_fdroid.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+# Change to the script's directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+
+# Build F-Droid compatible APK by removing non-FOSS dependencies
 flutter pub remove --offline patrol
+flutter pub remove --offline integration_test
 flutter pub remove --offline in_app_review
 flutter build apk --target lib/main_fdroid.dart
-flutter pub add --offline dev:patrol
-flutter pub add --offline in_app_review
+
+# Clean up
+git restore pubspec.lock pubspec.yaml
