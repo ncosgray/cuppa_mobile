b19a342d1cf38dea95274e043c337449e485ae1c
ncosgray <ncosgray@gmail.com>
2026-08-02 16:27:20 -0700

    more packages migrated to Swift PM

--- ios/RunnerUITests/RunnerUITests.m
+++ ios/RunnerUITests/RunnerUITests.m
@@ -2,4 +2,8 @@
 @import patrol;
 @import ObjectiveC.runtime;
 
+#if !defined(PATROL_INTEGRATION_TEST_IOS_RUNNER)
+#import "PatrolIntegrationTestIosRunner.h"
+#endif
+
 PATROL_INTEGRATION_TEST_IOS_RUNNER(RunnerUITests)
